272dc8135da953a97c975e7609e9b828e02c092d
SW <sw3159@columbia.edu>
2020-04-27 12:31:09 -0400

    lecture codes on graph algorithms

--- Introduction_to_Computational_Thinking_and_Data_Science/intro_graph_algo.py
+++ Introduction_to_Computational_Thinking_and_Data_Science/intro_graph_algo.py
@@ -85,4 +85,90 @@ class Graph(Digraph):
         rev = Edge(edge.getDestination(), edge.getSource())
         Digraph.addEdge(self, rev)
 
+##### Shortest Path problem 
+# 
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago', 'Denver', 'Phoenix', 'Los Angeles'):
+        g.addNode(Node(name))
+
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g 
+
+### DFS: depth first search 
+# avoid loops 
+
+#one example implementation of DFS 
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F', 'A'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+}
+
+visited = set() #keep track of visited nodes 
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(visited, graph, neighbor)
+
+#back to our example
+def DFS(graph, start, end, path = [], shortest = None, toPrint = False):
+    path = path + [start]
+    if toPrint:
+        print('Current DFS path: ', path)
+
+    if start == end:
+        print('DFS path: ', path)
+
+    for node in graph.childrenOf(start):
+        if node not in path: #avoid cycles
+            if shortest is None or len(path) < len(shortest):
+                newPath = DFS(graph, node, end, path, shortest, toPrint)
+                if newPath is not None:
+                    shortest = newPath 
+
+            elif toPrint:
+                print('Already visited ', node)
+    return shortest
+
+
+####BFS: breadth first search 
+def BFS(graph, start, end, toPrint=False):
+    initPath = [start]
+    pathQueue = [initPath] # a list of paths; something to explore
+    while len(pathQueue) > 0: #while I still get things to explore
+        #get and remove oldest element in pathQueue
+        tmpPath = pathQueue.pop(0)
+        if toPrint:
+            print('Current BFS path: ', path)
+        lastNode = tmpPath[-1]
+        if lastNode == end:
+            return tmpPath
+
+        for nextNode in graph.childrenOf(lastNode):
+            if nextNode not in tmpPath:
+                newPath = tmpPath + [nextNode]
+                pathQueue.append(newPath)
+
+    return None 
+
+
         
+
+
+   
